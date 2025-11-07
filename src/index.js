449e2fda2863aabc3e7f665ebc2bf7f3e10dfdd4
OscarStol12 <oscar.stolarczyk@yahoo.com>
2025-11-07 13:22:55 -0500

    Set things up for the bot runtime, and add the /ping command

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+"use strict";
+
+require('dotenv/config');
+const { Client, IntentsBitField } = require('discord.js');
+const { CommandHandler } = require('djs-commander');
+const path = require('path');
+
+const bot = new Client({ intents: [IntentsBitField.Flags.Guilds]})
+
+new CommandHandler({
+    client: bot,
+    commandsPath: path.join(__dirname, 'commands'),
+    eventsPath: path.join(__dirname, 'events'),
+    validationsPath: path.join(__dirname, 'validations')
+});
+
+bot.login(process.env.BOT_TOKEN);
